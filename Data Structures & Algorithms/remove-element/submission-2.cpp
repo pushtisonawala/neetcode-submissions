33dc1c2b44fb7895a2d7ccbfb8de19f9784e939c
pushtisonawala <168925733+pushtisonawala@users.noreply.github.com>
2026-05-19 20:30:06 +0530

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int> ans;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            while(i < nums.size() && nums[i] == val) {
+                i++;
+            }
+
+            if(i < nums.size()) {
+                ans.push_back(nums[i]);
+            }
+        }
+
+        nums = ans;
+
+        return ans.size();
+    }
+};
